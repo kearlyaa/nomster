63f29b5b1978830afd311f026e1659c27fe45842
Aaron K <kearlyaa@gmail.com>
2016-04-20 19:38:11 +0000

    Attempt to resolve precompiling assets failure in heroku

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Nomster
     # Add the fonts path
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
+    config.assets.initialize_on_precompile = false
+
+
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
 
